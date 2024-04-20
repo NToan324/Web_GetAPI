837daefc18c94c6cf47d6cce4ba76349ec4f4a66
Nhat Toan <nhattoan664t@gmail.com>
2024-04-20 17:53:06 +0700

    API

--- FE/Home/Home.js
+++ FE/Home/Home.js
@@ -2,9 +2,7 @@ function liked(x) {
     if (x.classList.contains('far')) {
         x.classList.remove('far');
         x.classList.add('fas');
-        x.classList.add('fill');
     } else {
-        x.classList.remove('fill');
         x.classList.remove('fas');
         x.classList.add('far');
     }
