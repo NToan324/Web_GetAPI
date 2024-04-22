e2bce5e349d7aed596b5333eae66348fcdf433bb
luhtri <luhtri2109@gmail.com>
2024-04-23 01:17:24 +0700

    feat: home controller

---
+++ src/controllers/home.controller.php
@@ -0,0 +1,11 @@
+<?php
+
+class HomeController {
+    public function showHome() {
+        if (!isset($_SESSION['email']) && !$_SESSION['logged_in']) {
+            
+        }
+        require_once __DIR__ . '/../views/Home/index.html';
+    }
+
+}
