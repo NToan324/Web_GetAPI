9dcf7a525d5f51466fca2563e569ecaf8708099d
luhtri <luhtri2109@gmail.com>
2024-04-25 00:00:03 +0700

    add basic method

--- src/models/Like.php
+++ src/models/Like.php
@@ -2,13 +2,22 @@
 
 class Like
 {
-    public function create()
+    private $conn;
+
+    public function __construct($conn)
     {
-        //
+        $this->conn = $conn;
+    }
+    
+    public function create($post_id, $user_id)
+    {
+        $stmt = $this->conn->prepare("INSERT INTO likes (post_id, user_id) VALUES (?, ?)");
+        return $stmt->execute([$post_id, $user_id]);
     }
 
-    public function delete()
+    public function delete($like_id)
     {
-        //
+        $stmt = $this->conn->prepare("DELETE FROM likes WHERE like_id = ?");
+        return $stmt->execute([$like_id]);
     }
 }
