97d5d007846a33ecf79c6f4508048a323d151cd7
nguyenn-04 <hatrongnguyen0423@gmail.com>
2024-04-24 21:34:09 +0700

    login

--- src/views/Login/login.js
+++ src/views/Login/login.js
@@ -1,34 +1,58 @@
-
 $(document).ready(function () {
-    let submitBtn = $("#login-submit-btn")
+    let submitBtn = $('#login-submit-btn')
     //bắt sự kiện click nút đăng nhập
     submitBtn.click(function (e) {
         // chặn hành vi gửi form mặc định của button
         e.preventDefault()
-        let email = $("#email").val()
-        let password = $("#password").val()
+        let email = $('#email').val()
+        let password = $('#password').val()
 
         // TODO: validate user input and show error
 
+        if (!email) {
+            $('#messageError-login').text('Email is required')
+            return
+        }
+
+        if (!validateEmail(email)) {
+            $('#messageError-login').text('Email is invalid')
+            return
+        }
+
+        if (!password) {
+            $('#messageError-login').text('Password is required')
+            return
+        }
+
+        if (password.length < 6) {
+            $('#messageError-login').text(
+                'Password must be at least 6 characters'
+            )
+            return
+        }
+
+        $('#messageError-login').text('')
+
         //call api kiểm tra đăng nhập
-        $.post(`/login`, { email, password, submit: true }, function (data) {
-            console.log(data)
-            if (!data.success) {
-                //không thành công thì hiện lỗi
-                $(".warningBox").text(data.message).addClass("shake-horizontal")
-                $(".txt_field").addClass('warning')
-                $(".label-input").addClass('text-warning')
-            } else {
-                window.location.href = '/'
-            }
-        }, "json");
+        $.post(
+            `/login`,
+            {email, password, submit: true},
+            function (data) {
+                console.log(data)
+                if (!data.success) {
+                    //không thành công thì hiện lỗi
+                    $('#messageError-login').text(data.message)
+                } else {
+                    window.location.href = '/'
+                }
+            },
+            'json'
+        )
     })
-
-    $(".txt_field input").keydown(function () {
-        $(".warningBox").text("").removeClass("shake-horizontal")
-        $(".label-input").removeClass('text-warning')
-        $(".txt_field").removeClass('warning')
-    })
-
-    
 })
+
+function validateEmail(email) {
+    var re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(email.toLowerCase())
+}
--- src/views/Login/signup.js
+++ src/views/Login/signup.js
@@ -1,47 +1,83 @@
-
 $(document).ready(function () {
-    let submitBtn = $("#signup-submit-btn")
+    let loginBtn = $('#login')
+    let submitBtn = $('#signup-submit-btn')
     //bắt sự kiện click nút đăng nhập
     submitBtn.click(function (e) {
         // chặn hành vi gửi form mặc định của button
         e.preventDefault()
-        let email = $("#signup-email").val()
-        let password = $("#signup-password").val()
-        let name = $("#signup-name").val()
+        let name = $('#signup-name').val()
+        let email = $('#signup-email').val()
+        let password = $('#signup-password').val()
 
-        // TODO: Validate user input
-        /* 
-            Kiểm tra input đầu vào của người dùng có hợp lệ không, không thì báo lỗi luôn
-        */
+        let emailLogin = $('#email').val()
+        let passwordLogin = $('#password').val()
 
-        // Call api đăng kí
-        $.post(`/signUp`, { name, email, password, submit: true }, function (res) {
-            console.log(res)
-            if (!res.success) { // Sign up failed
-                //* res.message là thông báo lỗi
-                // TODO: error when sign up, show error here
-                /* 
-                    Khi đăng kí lỗi thì hiển thị thông báo lỗi ở đây, lỗi lấy trong message ra
-                */
-                console.log(res.messagge)
-            } else { // Sign up successfully
-                // window.location.href = '/'
-                // * res.data là object chứa name, email, password
-                // TODO: back to login form with sign up data
-                /* 
-                    Chổ này đagnw nhập thành cônng nè, đem data quay về tragn login
-                */
+        if (!name) {
+            $('#messageError').text('Name is required')
+            return
+        }
 
+        if (!email) {
+            $('#messageError').text('Email is required')
+            return
+        }
+
+        if (!validateEmail(email)) {
+            $('#messageError').text('Email is invalid')
+            return
+        }
+
+        if (!password) {
+            $('#messageError').text('Password is required')
+            return
+        }
+
+        if (password.length < 6) {
+            $('#messageError').text('Password must be at least 6 characters')
+            return
+        }
+
+        $('#messageError').text('') 
+
+        // Call api đăng kí tài khoản
+        $.post(
+            `/signup`,
+            {name, email, password, submit: true},
+            function (res) {
                 console.log(res)
-            }
-        }, "json");
-    })
 
-    $(".txt_field input").keydown(function () {
-        $(".warningBox").text("").removeClass("shake-horizontal")
-        $(".label-input").removeClass('text-warning')
-        $(".txt_field").removeClass('warning')
-    })
+                if (!res.success) {
+                    // Sign up failed
+                    //* res.message là thông báo lỗi
+                    // TODO: error when sign up, show error here
+                    /* 
+                    Khi đăng kí lỗi thì hiển thị thông báo lỗi ở đây, lỗi lấy trong message ra
+                     */
+                    console.log(res.message)
+                } else {
+                    // Sign up successfully
+                    // window.location.href = '/'
+                    // * res.data là object chứa name, email, password
+                    // TODO: back to login form with sign up data
+                    /* 
+                    Chổ này đăng nhập thành công nè, đem data quay về trang login
+                    */
 
-    
+                    console.log(res)
+                }
+            },
+            'json'
+        )
+
+        loginBtn.click() // Chuyển về form đăng nhập
+        $('#email').val(email)
+        $('#password').val(password)
+        $('#messageError-login').text('Register successfully, please login')
+    })
 })
+
+function validateEmail(email) {
+    var re =
+        /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(email.toLowerCase())
+}
