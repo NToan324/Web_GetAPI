b9bbbe5d14eb5f432a23482d7ad25d1d2f42fdf6
luhtri <luhtri2109@gmail.com>
2024-04-26 10:49:15 +0700

    modify insert to match new data

--- src/db/db.sql
+++ src/db/db.sql
@@ -21,7 +21,7 @@ CREATE TABLE POSTS (
     post_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     content TEXT NOT NULL,
-    image VARCHAR(255),
+    image VARCHAR(255) DEFAULT 'default_avatar.jpg',
     total_likes INT DEFAULT 0,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -51,18 +51,19 @@ CREATE TABLE LIKES (
 );
 
 -- Chèn dữ liệu mẫu cho bảng USERS
-INSERT INTO USERS (name, email, password, avatar, bio, birthday)
+INSERT INTO USERS (name, email, password, avatar, bio, birthday) 
 VALUES 
-    ('User Name', 'user@example.com', 'password', 'avatar1.jpg', 'Lorem ipsum dolor sit amet.', '1990-01-01'),
-    ('Jane Smith', 'jane@example.com', 'password2', 'avatar2.jpg', 'Consectetur adipiscing elit.', '1985-05-15'),
-    ('Alice Johnson', 'alice@example.com', 'password3', 'avatar3.jpg', 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1995-11-20'),
-    ('Bob Brown', 'bob@example.com', 'password4', 'avatar4.jpg', 'Ut enim ad minim veniam.', '1980-09-10'),
-    ('Emily Taylor', 'emily@example.com', 'password5', 'avatar5.jpg', 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '1998-03-25'),
-    ('Michael Clark', 'michael@example.com', 'password6', 'avatar6.jpg', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '1992-07-12'),
-    ('Sarah White', 'sarah@example.com', 'password7', 'avatar7.jpg', 'Excepteur sint occaecat cupidatat non proident.', '1987-12-05'),
-    ('David Wilson', 'david@example.com', 'password8', 'avatar8.jpg', 'Sunt in culpa qui officia deserunt mollit anim id est laborum.', '1983-04-30'),
-    ('Olivia Martinez', 'olivia@example.com', 'password9', 'avatar9.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1991-06-18'),
-    ('James Lee', 'james@example.com', 'password10', 'avatar10.jpg', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '1989-08-22');
+    ('Luu Huu Tri', 'luhtri2109@gmail.com', '123456', 'avatar.jpg', 'Lorem ipsum dolor sit amet.', '1990-01-01'),
+    ('Jane Smith', 'jane@example.com', 'password2', 'default_avatar.jpg', 'Consectetur adipiscing elit.', '1985-05-15'),
+    ('User Name', 'user@example.com', 'password', 'default_avatar.jpg', 'Lorem ipsum dolor sit amet.', '1990-01-01'),
+    ('Alice Johnson', 'alice@example.com', 'password3', 'default_avatar.jpg', 'Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1995-11-20'),
+    ('Bob Brown', 'bob@example.com', 'password4', 'default_avatar.jpg', 'Ut enim ad minim veniam.', '1980-09-10'),
+    ('Emily Taylor', 'emily@example.com', 'password5', 'default_avatar.jpg', 'Quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '1998-03-25'),
+    ('Michael Clark', 'michael@example.com', 'password6', 'default_avatar.jpg', 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.', '1992-07-12'),
+    ('Sarah White', 'sarah@example.com', 'password7', 'default_avatar.jpg', 'Excepteur sint occaecat cupidatat non proident.', '1987-12-05'),
+    ('David Wilson', 'david@example.com', 'password8', 'default_avatar.jpg', 'Sunt in culpa qui officia deserunt mollit anim id est laborum.', '1983-04-30'),
+    ('Olivia Martinez', 'olivia@example.com', 'password9', 'default_avatar.jpg', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', '1991-06-18'),
+    ('James Lee', 'james@example.com', 'password10', 'default_avatar0.jpg', 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', '1989-08-22');
 
 -- Chèn dữ liệu mẫu cho bảng POSTS
 INSERT INTO POSTS (user_id, content, image)
@@ -71,12 +72,12 @@ VALUES
     (2, 'This is the content of post 2.', 'image2.jpg'),
     (3, 'This is the content of post 3.', 'image3.jpg'),
     (4, 'This is the content of post 4.', 'image4.jpg'),
-    (5, 'This is the content of post 5.', 'image5.jpg'),
-    (6, 'This is the content of post 6.', 'image6.jpg'),
-    (7, 'This is the content of post 7.', 'image7.jpg'),
-    (8, 'This is the content of post 8.', 'image8.jpg'),
-    (9, 'This is the content of post 9.', 'image9.jpg'),
-    (10, 'This is the content of post 10.', 'image10.jpg');
+    (5, 'This is the content of post 5.', 'image2.jpg'),
+    (6, 'This is the content of post 6.', 'image3.jpg'),
+    (7, 'This is the content of post 7.', 'image4.jpg'),
+    (8, 'This is the content of post 8.', 'image1.jpg'),
+    (9, 'This is the content of post 9.', 'image2.jpg'),
+    (10, 'This is the content of post 10.', 'image1.jpg');
 
 -- Chèn dữ liệu mẫu cho bảng COMMENTS
 INSERT INTO COMMENTS (post_id, user_id, content)
