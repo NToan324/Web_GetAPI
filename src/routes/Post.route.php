34f87a16bb5340b96fa9ee3316bb474f570465d4
luhtri <luhtri2109@gmail.com>
2024-04-26 12:12:54 +0700

    routing

---
+++ src/routes/Post.route.php
@@ -0,0 +1,18 @@
+<?php
+require_once SERVER_PATH . '/controllers/Post.controller.php';
+
+
+
+$request = $_SERVER['REQUEST_URI'];
+$split_request = explode("?", $request);
+$request_parts = explode('/', $split_request[0]);
+
+$postController = new PostController();
+
+
+switch ($request_parts[1]) {
+    case 'loadAllPost': {
+            $postController->loadAllPost();
+            break;
+        }
+}
