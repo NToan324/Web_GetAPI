f41b00e3c9728f97f41ef6c28e7cfbbfffed5e18
nguyenn-04 <hatrongnguyen0423@gmail.com>
2024-05-25 16:22:23 +0700

    popup

--- src/controllers/AccountController.php
+++ src/controllers/AccountController.php
@@ -57,8 +57,8 @@ class AccountController
     {
         header('Content-type: application/json');
 
-        // $userId = $_SESSION['id'];
-        $userId = "2";
+        $userId = $_SESSION['id'];
+        // $userId = "2";
 
         try {
             $posts = Post::getAllPostOfUser($userId);
