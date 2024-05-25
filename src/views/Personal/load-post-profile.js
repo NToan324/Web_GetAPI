343ba27127ef52292031b483e6c9ef53e25ed9d1
nguyenn-04 <hatrongnguyen0423@gmail.com>
2024-05-25 16:21:53 +0700

    popup

--- src/views/Personal/load-post-profile.js
+++ src/views/Personal/load-post-profile.js
@@ -4,69 +4,107 @@ function renderPosts(posts, user) {
         const dateB = new Date(b.created_at);
         return dateB - dateA; // Later created time comes first (descending)
     });
-    const postContainer = $('.personal-page-post')
+    const postContainer = $('.personal-page-post');
     posts.forEach((post) => {
-        let createdAt = new Date(post.created_at)
-        let now = new Date()
-        let diffInHours = Math.abs(now - createdAt) / 36e5
-        let time = ''
+        let createdAt = new Date(post.created_at);
+        let now = new Date();
+        let diffInHours = Math.abs(now - createdAt) / 36e5;
+        let time = '';
 
         if (diffInHours < 24) {
-            time = Math.floor(diffInHours) + ' hours ago'
+            time = Math.floor(diffInHours) + ' hours ago';
         } else {
-            time = createdAt.toLocaleDateString()
+            time = createdAt.toLocaleDateString();
         }
 
-        // TODO: thêm nút xoá cho mỗi post -> rồi qua file delete-post.js
         let html = `
-                    <div class="post-box" data-id="${post.id}">
-                    <div class="post-profile">
-                        <div class="post-img">
-                            <img src="/Web_RestAPI/storage/users/${post.avatar}" alt="" />
-                        </div>
-                        <div class="post-name">
-                            <h3>${post.user_name}</h3>
-                            <p>${time}</p>
-                        </div>
+            <div class="post-box" data-id="${post.id}">
+                <div class="post-profile">
+                    <div class="post-img">
+                        <img src="/Web_RestAPI/storage/users/${post.avatar}" alt="" />
                     </div>
+                    <div class="post-name">
+                        <h3>${post.user_name}</h3>
+                        <p>${time}</p>
+                    </div>
+                </div>
 
-                    <img src="/Web_RestAPI/storage/posts/${post.image}" alt="" />
-                    <div class="post-info">
-                        <div class="likes">
-                            <i class="far fa-heart" onclick="liked(this)"></i>
-                        </div>
-                        <div class="comments">
-                            <i class="far fa-comment-dots"></i>
-                        </div>
-                        <div class="delete-post">
-                            <i class="far fa-trash-alt" onclick="deletePost()"></i>
+                <img src="/Web_RestAPI/storage/posts/${post.image}" alt="" />
+                <div class="post-info">
+                    <div class="likes">
+                        <i class="far fa-heart" onclick="liked(this)"></i>
+                    </div>
+                    <div class="comments">
+                        <i class="far fa-comment-dots"></i>
+                    </div>
+                    <div class="edit-post">
+                        <i class="far fa-edit" onclick="editPost(this)"></i>
+                    </div>
+                    <div class="delete-post">
+                        <i class="far fa-trash-alt" onclick="deletePost()"></i>
+                    </div>
+                </div>
+                <div class="likes-comments-info">
+                    <span>${post.total_likes} Likes</span>
+                    <div class="comments-show">
+                        <div class="comments-user">
+                            <h4>${post.user_name}</h4>
+                            <p class="post-content" data-content="${post.content}">${post.content}</p>
                         </div>
                     </div>
-                    <div class="likes-comments-info">
-                        <span>${post.total_likes} Likes</span>
-                        <div class="comments-show">
-                            <div class ="comments-user">
-                                <h4>${post.user_name}</h4>
-                                <p>${post.content}</p>
-                            </div>  
+                    <form>
+                        <div class="write-comments">
+                            <input type="text" placeholder="Write a comment..." />
+                            <button type="submit"><i class="far fa-paper-plane"></i></button>
                         </div>
-                        <form>
-                            <div class="write-comments">
-                                <input type="text" placeholder="Write a comment..." />
-                                <button type="submit"><i class="far fa-paper-plane"></i></button>
-                            </div>
-                        </form>
-                    </div>
-                `
+                    </form>
+                </div>
+            </div>
+        `;
 
-        postContainer.append(html)
-    })
+        postContainer.append(html);
+    });
+}
+
+function editPost(element) {
+    const postBox = $(element).closest('.post-box');
+    const contentElement = postBox.find('.post-content');
+    const originalContent = contentElement.data('content');
+    const formElement = $(`
+        <form class="edit-form">
+            <input type="text" class="edit-input" value="${originalContent}"/>
+            <button type="button" class="save-button">Save</button>
+        </form>
+    `);
+
+    contentElement.replaceWith(formElement);
+
+    const inputElement = formElement.find('.edit-input');
+    inputElement.focus();
+
+    formElement.on('submit', function (event) {
+        event.preventDefault();
+    });
+
+    formElement.find('.save-button').on('click', function () {
+        const newContent = inputElement.val();
+        formElement.replaceWith(`<p class="post-content" data-content="${newContent}">${newContent}</p>`);
+        
+        // TODO: Gọi API để cập nhật nội dung mới lên server
+    });
+
+    inputElement.on('blur', function () {
+        const newContent = inputElement.val();
+        formElement.replaceWith(`<p class="post-content" data-content="${newContent}">${newContent}</p>`);
+        
+        // TODO: Gọi API để cập nhật nội dung mới lên server
+    });
 }
 
 $.get('/Web_RestAPI/load-profile', (res) => {
     if (res.success) {
-        renderPosts(res.data.posts, res.data.user)
+        renderPosts(res.data.posts, res.data.user);
     } else {
-        console.log(res)
+        console.log(res);
     }
-})
+});
